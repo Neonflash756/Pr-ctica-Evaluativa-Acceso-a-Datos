4a1e2b72045ec3dbcee0fbb7309bfddb0f8fd357
xp <xp@CETYSDAM00>
2022-04-25 14:20:09 +0200

    Commit inicial

---
+++ src/práctica/evaluativa/PrácticaEvaluativa.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package práctica.evaluativa;
+
+/**
+ *
+ * @author xp
+ */
+public class PrácticaEvaluativa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
