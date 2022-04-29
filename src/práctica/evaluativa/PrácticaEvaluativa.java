3c22ac359470d6b9b2baf1544579a0e1dcf0855b
xp <xp@CETYSDAM00>
2022-04-29 18:23:59 +0200

    Finish

--- src/práctica/evaluativa/PrácticaEvaluativa.java
+++ src/práctica/evaluativa/PrácticaEvaluativa.java
@@ -21,22 +21,16 @@ public class PrácticaEvaluativa {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/tienda","root","");
-        ResultSet rs;
+        
         Scanner sc = new Scanner(System.in);
         System.out.print("QUIERE ADICIONAR NUEVOS DATOS A LA BASO DE DATOS? \n"
-                + "SI=1 " + "NO=2");
+                + "SI=1 " + "NO=2 \n");
         String respuesta = sc.nextLine();
         int numero = Integer.parseInt(respuesta);
         
         while(numero == 1){
             try (Statement st = conexion.createStatement()) { 
-                
-                System.out.print("QUIERE INSERTAR UN FABRICANTE? \n"
-                + "SI = 1 " + "NO = 2" );
-                String r1 = sc.nextLine();
-                int n1 = Integer.parseInt(respuesta);
-                
-                if(n1 ==1){
+
                     System.out.print("INTRODUZCA NOMBRE DEL FABRICANTE: ");
                     String nombreFabricante = sc.nextLine();
 
@@ -44,20 +38,18 @@ public class PrácticaEvaluativa {
                     ps.setString(1, nombreFabricante);
                     ps.executeUpdate();
                     ps.close();
-                }
                 
-                System.out.print("QUIERE INSERTAR UN PRODUCTO? \n"
-                + "SI = 1 " + "NO = 2" );
-                String r2 = sc.nextLine();
-                int n2 = Integer.parseInt(respuesta);
-                if(n2 ==1){
-                    System.out.println("INTRODUZCA NOMBRE DEL PRODUCTO: ");
+                    System.out.println("INTRODUZCA NOMBRE DEL PRODUCTO:\n");
                     String nombreProducto = sc.nextLine();
 
-                    System.out.print("INTRODUZCA PRECIO DEL PRODUCTO: ");
+                    System.out.print("INTRODUZCA PRECIO DEL PRODUCTO:\n");
                     String precio = sc.nextLine(); 
-
-                    String codigoFabricante = "SELECT `codigo` FROM `fabricante` WHERE nombre LIKE 'nombreFabricante'";
+                    
+                    String consultaSql = "SELECT MAX(codigo) AS codigo FROM `fabricante`";
+                    ResultSet rs = st.executeQuery(consultaSql);
+                    rs.next();
+                    
+                    String codigoFabricante = String.valueOf(rs.getInt("codigo"));
 
                     PreparedStatement PS = conexion.prepareStatement("INSERT INTO producto (nombre, precio ,codigo_fabricante) values(?,?,?)");
                     PS.setString(1, nombreProducto);
@@ -65,16 +57,16 @@ public class PrácticaEvaluativa {
                     PS.setString(3, codigoFabricante);
                     PS.executeUpdate();
                     PS.close(); 
-                }
+                
             }
             
             System.out.print("QUIERE ADICIONAR NUEVOS DATOS A LA BASO DE DATOS? \n"
-                + "SI=1 " + "NO=2");
+                + "SI=1 " + "NO=2\n");
             respuesta = sc.nextLine();
             numero = Integer.parseInt(respuesta);
         }
         
-        System.out.print("PROGRAMA FINALIZADO \n");
+        System.out.print("PROGRAMA FINALIZADO\n");
     }
     
 }
